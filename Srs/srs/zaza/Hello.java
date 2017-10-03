83b32de07c696cdfe435fee9dff93c63809289be
Zaya <zarina.kost11@gmail.com>
2017-10-04 04:50:28 +0600

    experiment1 bytaginda Hello.java failina ozgerister jasaldi.

--- Srs/srs/zaza/Hello.java
+++ Srs/srs/zaza/Hello.java
@@ -6,6 +6,7 @@ public class Hello {
         System.out.println("Byl zertxanalik jumis macsaty");
         System.out.println("GitHub-pen jumis jasau");
 
+<<<<<<< Updated upstream
         int a = MyMath1.kvadrat(a=5);
         int b = MyMath1.kub(a=5) ;
 
@@ -13,6 +14,17 @@ public class Hello {
         System.out.println("b = " + b) ;
 
         System.out.println(MyMath1.max(a=8,b=9));
+=======
+
+        System.out.println("a= " +a);
+        System.out.println("b= " +b);
+
+        System.out.println("max =" + MyMath1.max3(a=8,b=9, c=12));
+
+        System.out.println(MyMath1.isEven(a=12));
+        System.out.println(MyMath1.isEven(a==7));
+
+>>>>>>> Stashed changes
     }
     }
 
