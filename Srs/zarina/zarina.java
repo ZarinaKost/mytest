13a2f3a279670476dec5f14750cd48c39f65d98d
Zaya <zarina.kost11@gmail.com>
2017-10-04 01:54:14 +0600

    Екінші коммит

--- Srs/zarina/zarina.java
+++ Srs/zarina/zarina.java
@@ -3,5 +3,7 @@ package zarina;
 public class zarina {
     public static void main(String[] args){
         System.out.println("Hello Zaika");
+        System.out.println("Byl zertxanalik jumis macsaty");
+        System.out.println("GitHub-pen jumis jasau");
     }
 }
