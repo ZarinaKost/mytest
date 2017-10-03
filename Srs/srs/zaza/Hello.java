efe515f949b777a0410e58281ad3ea10d6f5ed6f
Zaya <zarina.kost11@gmail.com>
2017-10-04 04:27:58 +0600

    Hello.java  failnda ozgeris

--- Srs/srs/zaza/Hello.java
+++ Srs/srs/zaza/Hello.java
@@ -5,5 +5,14 @@ public class Hello {
         System.out.println("Hello Zaika");
         System.out.println("Byl zertxanalik jumis macsaty");
         System.out.println("GitHub-pen jumis jasau");
+
+        int a = MyMath1.kvadrat(a=5);
+        int b = MyMath1.kub(a=5) ;
+
+        System.out.println("a = " + a) ;
+        System.out.println("b = " + b) ;
+
+        System.out.println(MyMath1.max(a=8,b=9));
     }
-}
+    }
+
