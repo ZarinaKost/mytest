d1d347377f37b18ef3e5084e6e2b8b543089c973
Zaya <zarina.kost11@gmail.com>
2017-10-04 04:11:55 +0600

    Birinwi kommit

--- Srs/zarina/zarina.java
+++ Srs/zarina/zarina.java
@@ -5,5 +5,11 @@ public class zarina {
         System.out.println("Hello Zaika");
         System.out.println("Byl zertxanalik jumis macsaty");
         System.out.println("GitHub-pen jumis jasau");
+
+        int a = MyMath.kvadrat(a=5);
+        int b = MyMath.kub(a=5) ;
+
+        System.out.println("a = " + a) ;
+        System.out.println("b = " + b) ;
     }
 }
